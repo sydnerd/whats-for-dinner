4fd26d417009b73029c0cb0913767078ac6dac3d
Sydney Bear <sbear3915@gmail.com>
2021-04-08 14:06:08 -0600

    Add hidden class

---
+++ main.js
@@ -0,0 +1,9 @@
+//when the user clicks a dish option, and then clicks lets cook,
+//the user sees a randomdish from the list of possible dishes
+//create variables with array of dish options for side, main dish
+//and Dessert
+//use query selector to grab html that we are going to be using
+//create event listener on lets cook button
+//create random generator function
+//random generated meal comes on the cookpot box,
+//put hidden class on the pot
