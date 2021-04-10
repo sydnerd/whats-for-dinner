4042ed7cd892080cb6a1e6762d1cfe7209618731
Sydney Bear <sbear3915@gmail.com>
2021-04-10 14:49:04 -0600

    refactor CSS

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-
-
-//list of queryselectors
-
 var radioButtons = document.querySelectorAll(".radioButtons");
 var letsCookButton = document.querySelector("#cookButton");
 var randomDishDisplay = document.querySelector("#dishDisplay");
@@ -11,44 +7,12 @@ var rightText =document.querySelector("#rightText");
 
 
 
-//list of event listners
+
 letsCookButton.addEventListener("click",displayMeal);
 
 
 
-// function displayMeal(){
-//   var recipe = sides[getRandomIndex(recipe)]
-//   if(sideRadio.checked){
-//     recipe = sides[getRandomIndex(sides)]
-//     randomDishDisplay.innerText =recipe;
-//   }
-// }
-// function showRecipe(recipe){
-//   randomDishDisplay.innerText =
-//     `<h2>${recipe}</h2>`
-//   }
-//would like to refactor this to be smaller and similer, maybe can use some interpolation
-//need to figure out how to get the "You should make: text to appear
-//need to figure out how to interpolate the exclamation mark after the food
 
-// function displayMeal(){
-//   for(var i=0; i<radioButtons.length; i++)
-//     if(radioButtons[i].checked){
-//     console.log(radioButtons[i])
-//     }
-// var sideList = sides[getRandomIndex(sides)];
-// var mainsList = mains[getRandomIndex(mains)];
-// var dessertList = desserts[getRandomIndex(desserts)];
-// rightText.classList.remove("hidden");
-// image.classList.add("hidden");
-//   if(sideRadio.checked){
-//   randomDishDisplay.innerText =`${sideList}!`;
-//   }else if(mainRadio.checked){
-//   randomDishDisplay.innerText =`${mainsList}!`;
-//   }else {
-//   randomDishDisplay.innerText =`${dessertList}!`;
-//   }
-// }
   function buttonClicked(){
   for(var i=0; i<radioButtons.length; i++)
     if(radioButtons[i].checked){
@@ -59,20 +23,16 @@ letsCookButton.addEventListener("click",displayMeal);
 function sideDisplay(){
   var sideList = sides[getRandomIndex(sides)];
   return sideList;
-  // randomDishDisplay.innerText =`${sideList}!`;
 }
 
 function dessertDisplay(){
   var dessertList = desserts[getRandomIndex(desserts)];
   return dessertList;
-  // randomDishDisplay.innerText =`${dessertList}!`;
 }
 
 function mainsDisplay(){
   var mainsList = mains[getRandomIndex(mains)];
   return mainsList;
-
-  // randomDishDisplay.innerText =`${mainsList}!`;
 }
 
 function displayMeal(){
@@ -89,11 +49,10 @@ function displayMeal(){
   }if(button == "entireMeal"){
     text = `${mainsDisplay()} with a side of ${sideDisplay()} and ${dessertDisplay()} for dessert`
   }
-  randomDishDisplay.innerText = `${text} !`
+  randomDishDisplay.innerText = `${text}!`
 }
 
 
-//List of function for random dishes
 var sideList = sides[getRandomIndex(sides)];
 var mainsList = mains[getRandomIndex(mains)];
 var dessertList = mains[getRandomIndex(desserts)];
